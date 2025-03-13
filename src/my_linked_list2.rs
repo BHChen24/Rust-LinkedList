2fa3be5e7dad8fddc0fdd497950d3db136c26c46
Baihua Chen <bchen102@myseneca.ca>
2025-03-13 11:26:31 -0400

    Iterator

--- src/my_linked_list2.rs
+++ src/my_linked_list2.rs
@@ -57,8 +57,13 @@ pub struct MyLinkedList2<T> {
 
 impl<T> MyLinkedList2<T> {
     pub fn new() -> Self {
-        let head_sentinel = NonNull::new(Box::into_raw(Box::new(Node::new())));
-        let tail_sentinel = NonNull::new(Box::into_raw(Box::new(Node::new())));
+        let mut head_sentinel = NonNull::new(Box::into_raw(Box::new(Node::new())));
+        let mut tail_sentinel = NonNull::new(Box::into_raw(Box::new(Node::new())));
+
+        unsafe {
+            head_sentinel.unwrap().as_mut().next = tail_sentinel;
+            tail_sentinel.unwrap().as_mut().prev = head_sentinel;
+        }
 
         Self {
             size: 0,
@@ -74,6 +79,14 @@ impl<T> MyLinkedList2<T> {
 
     pub fn get_size(&self) -> usize {
         self.size
+    }
+
+    fn _get_front(&self) -> Option<NonNull<Node<T>>> {
+        unsafe { self.head.unwrap().as_ref().next }
+    }
+
+    fn _get_back(&self) -> Option<NonNull<Node<T>>> {
+        unsafe { self.tail.unwrap().as_ref().prev }
     }
 
     fn check_available_index(&self, idx: usize) -> Result<(), Box<dyn Error>> {
@@ -96,7 +109,7 @@ impl<T> MyLinkedList2<T> {
         }
 
         unsafe {
-            let mut front = self.head.unwrap().as_ref().next;
+            let mut front = self._get_front();
             let mut helper = front.unwrap().as_ref().next;
 
             let front_box = Box::from_raw(front.unwrap().as_ptr());
@@ -116,8 +129,8 @@ impl<T> MyLinkedList2<T> {
         }
 
         unsafe {
-            let mut back = self.tail.unwrap().as_ref().next;
-            let mut helper = back.unwrap().as_ref().next;
+            let mut back = self._get_back();
+            let mut helper = back.unwrap().as_ref().prev;
 
             let back_box = Box::from_raw(back.unwrap().as_ptr());
             let return_data = back_box.data;
@@ -127,6 +140,60 @@ impl<T> MyLinkedList2<T> {
 
             self.size -= 1;
             Ok(return_data)
+        }
+    }
+
+    pub fn push_front(&mut self, data: T) {
+        let mut new_node = Box::new(Node::new_with(data));
+
+        let mut front = self._get_front();
+        new_node.next = front;
+        new_node.prev = self.head;
+
+        unsafe {
+            let new_node_ptr = NonNull::new(Box::into_raw(new_node));
+            front.unwrap().as_mut().prev = new_node_ptr;
+            self.head.unwrap().as_mut().next = new_node_ptr;
+
+            self.size += 1;
+        }
+    }
+
+    pub fn push_back(&mut self, data: T) {
+        let mut new_node = Box::new(Node::new_with(data));
+
+        let mut back = self._get_back();
+        new_node.prev = back;
+        new_node.next = self.tail;
+
+        unsafe {
+            let new_node_ptr = NonNull::new(Box::into_raw(new_node));
+            back.unwrap().as_mut().prev = new_node_ptr;
+            self.tail.unwrap().as_mut().next = new_node_ptr;
+
+            self.size += 1;
+        }
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter{ list: self }
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter {
+            front: self._get_front(),
+            back: self._get_back(),
+            size: self.size,
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {
+            front: self._get_front(),
+            back: self._get_back(),
+            size: self.size,
+            _marker: PhantomData,
         }
     }
 }
@@ -139,21 +206,49 @@ impl<T> Iterator for IntoIter<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.pop_front()
+        self.list.pop_front().ok()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.list.size, Some(self.list.size))
+    }
+}
+
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.list.pop_back().ok()
+    }
+}
+
+impl<T> Drop for IntoIter<T> {
+    fn drop(&mut self) {
+        for _ in &mut *self {}
     }
 }
 
 struct Iter<'a, T> {
     size: usize,
-    head: Option<NonNull<Node<T>>>,
-    tail: Option<NonNull<Node<T>>>,
+    front: Option<NonNull<Node<T>>>,
+    back: Option<NonNull<Node<T>>>,
     _marker: PhantomData<&'a T>,
+}
+
+impl <'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.size, Some(self.size))
+    }
 }
 
 struct IterMut<'a, T> {
     size: usize,
-    head: Option<NonNull<Node<T>>>,
-    tail: Option<NonNull<Node<T>>>,
+    front: Option<NonNull<Node<T>>>,
+    back: Option<NonNull<Node<T>>>,
     _marker: PhantomData<&'a mut T>,
 }
 
