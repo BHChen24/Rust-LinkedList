064122f7ed3779f1b4f5667f503825da1c6a220e
Baihua Chen <bchen102@myseneca.ca>
2025-03-21 10:38:41 -0400

    Modified work files

--- scripts/helloworld.py
+++ scripts/helloworld.py
@@ -1,11 +1,94 @@
-class Mytree:
-    pass
+class MyTree:
 
-class MyHeap:
-    pass
+    class TreeNode:
+        def __init__(self, data = None):
+            self.data = data
+            self.left = None
+            self.right = None
+
+    def __init__(self, size = 1):
+        self.root = MyTree.TreeNode()
+        self.size = size
 
 class MyGraph:
-    pass
+
+    class Edge:
+        def __init__(self):
+            self.to = None
+            self.weight = None
+
+    def __init__(self):
+        self.adjacency_table = {}
 
 class MyBST:
-    pass
+    class BSTreeNode:
+
+        def __init__(self, data = None):
+            self.data = data
+            self.left = None
+            self.right = None
+            self.size = 0
+
+    def __init__(self):
+        self.root = self.BSTreeNode()
+
+    def search(self, node, target):
+        if node is None:
+            return False
+
+        if node.data == target:
+            return True
+
+        if target < node.data:
+            return self.search(node.left, target)
+        else:
+            return self.search(node.right, target)
+
+    def search_iterative(self, target):
+
+        node = self.root
+
+        while node is not None:
+            if node.data == target:
+                return True
+            elif target < node.data:
+                node = node.left
+            else:
+                node = node.right
+
+        return False
+
+
+    def put(self, data):
+        pass
+
+    def remove(self):
+        pass
+
+    def max(self):
+        pass
+
+    def min(self):
+        pass
+
+    def sorted_list(self):
+        pass
+
+    def flooring(self, value):
+        pass
+
+    def ceiling(self, value):
+        pass
+
+    def rank(self, rank):
+        pass
+
+    def _right_rotate(self, node):
+        pass
+
+    def _left_rotate(self, node):
+        pass
+
+class MyHeap:
+    def __init__(self):
+        self.heap = []
